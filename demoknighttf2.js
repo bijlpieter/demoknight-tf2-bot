5e093b0a7651fe5d85635bf0e7c242c5132a172a
Chrysophylaxs <43391350+Chrysophylaxs@users.noreply.github.com>
2019-09-30 02:35:39 +0200

    Add files via upload

--- demoknighttf2.js
+++ demoknighttf2.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client({disableEveryone: true});
-let demoknighttf2 = 313;
 
 client.on("ready", function() {
 	client.user.setActivity('demoknight tf2', {type: 'PLAYING'});
@@ -9,22 +8,9 @@ client.on("ready", function() {
 });
 
 client.on('message', async msg => {
-	let args = msg.content.split(' ');
-	for (let i = 0; i < args.length - 1; i++) {
-		if (args[i] == "demoknight" && args[i + 1] == "tf2") {
-			demoknighttf2++;
-		}
-	}
 	if (msg.content.startsWith('!demoknighttf2')) {
-		msg.channel.send(msg.author.displayName + " has praised the holy demoknight team fortress 2");
+		msg.channel.send(msg.member.displayName + " has praised the holy demoknight team fortress 2");
 	}
-	else if (msg.content.startsWith("!say")) {
-		args = args.slice(1);
-    	let sayMessage = args.join(" ");
-    	msg.delete(); 
-   		msg.channel.send(sayMessage);
-	}
-
 	return undefined;
 });
 
