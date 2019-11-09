c5230390cc9f5e89f66847aab03d4c44dc347a8f
Chrysophylaxs <43391350+Chrysophylaxs@users.noreply.github.com>
2019-11-09 03:31:06 +0100

    Add files via upload

--- demoknighttf2.js
+++ demoknighttf2.js
@@ -1,7 +1,7 @@
-const Discord = require('discord.js');
+const discord = require('discord.js');
 const tmi = require('tmi.js');
-
-const client = new Discord.Client({disableEveryone: true});
+const fs = require('fs');
+const config = require('./config.json');
 
 const options = {
 	connection: {
@@ -9,33 +9,91 @@ const options = {
 		secure: true,
 	},
 	options: {
-		debug: true,
+		debug: false,
 	},
 	identity: {
 		username: "demoknight_tf2",
-		password: "oauth:q7det5pjtc8q6vqfcbtc6vclqlmz8o"
+		password: process.env.TWITCH
 	},
-	channels: ['SolarLightTF2']
+	channels: ['SolarLightTF2', 'chrysophylaxss']
 };
 
+const dclient = new discord.Client({disableEveryone: true});
 const tclient = new tmi.client(options);
 tclient.connect();
-tclient.on('chat', function(channel, userstate, message, self) {
-	if (message == "!demoknighttf2") {
-		tclient.say(channel, userstate['display-name'] + " has praised the holy demoknight team fortress 2");
-	}
-});
 
-client.on("ready", function() {
-	client.user.setActivity('demoknight tf2', {type: 'PLAYING'});
+dclient.on("ready", function() {
+	dclient.user.setActivity('demoknight tf2', {type: 'PLAYING'});
 	console.log("demoknight tf2");
 });
 
-client.on('message', async msg => {
-	if (msg.content.startsWith('!demoknighttf2')) {
-		msg.channel.send(msg.member.displayName + " has praised the holy demoknight team fortress 2");
+let rawdata = fs.readFileSync('commands.json');
+let commands = JSON.parse(rawdata);
+
+tclient.on('chat', (channel, userstate, message, self) => {
+	if (self) return;
+	if (message.startsWith('!addcomm') && (userstate.badges.hasOwnProperty('moderator') || userstate.badges.hasOwnProperty('broadcaster'))) {
+		if (newCommand(message)) tclient.say(channel, "Added or updated command!");
+		else tclient.say(channel, "Could not add command!");
+	}
+	else if (message.startsWith('!delcomm') && userstate['mod'] == true) {
+		if (delCommand(message)) tclient.say(channel, "Removed command!");
+		else tclient.say(channel, "Could not remove command!");
+	}
+	else if (message.startsWith('!')) {
+		let msg = message.toLowerCase();
+		if (commands.hasOwnProperty(msg)) {
+			response = commands[msg];
+			if (msg.startsWith("!demoknight")) response = userstate['display-name'] + response;
+			tclient.say(channel, response);
+		}
+	}
+});
+
+dclient.on('message', (message) => {
+	if (message.content.startsWith('!addcomm') && message.member.hasPermission('MANAGE_MESSAGES', true, true, true)) {
+		if (newCommand(message.content)) message.channel.send("Added or updated command!");
+		else message.channel.send("Could not add command!");
+	}
+	else if (message.content.startsWith('!delcomm') && message.member.hasPermission('MANAGE_MESSAGES', true, true, true)) {
+		if (delCommand(message.content)) message.channel.send("Removed command!");
+		else message.channel.send("Could not remove command!");
+	}
+	else if (message.content.startsWith('!')) {
+		let msg = message.content.toLowerCase();
+		if (commands.hasOwnProperty(msg)) {
+			response = commands[msg];
+			if (msg.startsWith("!demoknight")) response = message.member.displayName + response;
+			message.channel.send(response);
+		}
 	}
 	return undefined;
 });
 
-client.login(process.env.TOKEN);
+function newCommand(msg) {
+	let args = msg.split(' ');
+	if (args.length < 3) return false;
+	let newComm = args[1];
+	args = args.splice(2);
+	commands["!" + newComm] = args.join(" ");
+	if (commands.hasOwnProperty("!" + newComm)) commands["!commands"] += ", !" + newComm;
+	let data = JSON.stringify(commands, null, 4);
+	fs.writeFileSync('commands.json', data);
+	return true;
+}
+
+function delCommand(msg) {
+	let args = msg.split(' ');
+	let delComm = args[1];
+	if (delComm == "commands") return false;
+	if (commands.hasOwnProperty("!" + delComm)) {
+		delete commands["!" + delComm];
+		commands["!commands"] = commands["!commands"].replace(", !" + delComm, "");
+		let data = JSON.stringify(commands, null, 4);
+		fs.writeFileSync('commands.json', data);
+		return true;
+	}
+	else return false;
+}
+
+dclient.login(process.env.TOKEN);
