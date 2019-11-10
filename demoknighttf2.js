076930e017da0e95707b58461879442d3a543e60
Chrysophylaxs <43391350+Chrysophylaxs@users.noreply.github.com>
2019-11-10 15:49:28 +0100

    Add files via upload

--- demoknighttf2.js
+++ demoknighttf2.js
@@ -39,57 +39,49 @@ dclient.on("ready", function() {
 tclient.on('chat', (channel, userstate, message, self) => {
 	if (message.startsWith("!") && !self) {
 		let response = handleCommand(message.toLowerCase(), userstate['display-name'], userstate.badges.hasOwnProperty('moderator') || userstate.badges.hasOwnProperty('broadcaster'));
-		if (response != "") tclient.say(channel, response);
+		if (response) tclient.say(channel, response);
 	}
 });
 
 dclient.on('message', (message) => {
 	if (message.content.startsWith("!") && !message.author.bot) {
 		let response = handleCommand(message.content.toLowerCase(), message.member.displayName, message.member.hasPermission('MANAGE_MESSAGES', true, true, true));
-		if (response != "") message.channel.send(response);
+		if (response) message.channel.send(response);
 	}
 	return undefined;
 });
 
 function handleCommand(msg, name, mod) {
-	if (msg.startsWith('!addcomm') && mod) {
-		if (newCommand(msg)) return "Added or updated command!";
-		else return "Could not add command!";
-	}
-	else if (msg.startsWith('!delcomm') && mod) {
-		if (delCommand(msg)) return "Removed command!"
-		else return "Could not remove command!";
-	}
-	else if (commands.hasOwnProperty(msg)) {
-		if (msg.startsWith("!demoknight")) return name + commands[msg];
-		else return commands[msg];
-	}
+	if (msg.startsWith('!addcomm') && mod) return newCommand(msg);
+	else if (msg.startsWith('!delcomm') && mod) return delCommand(msg);
+	else if (msg.startsWith("!demoknight")) return name + commands["!demoknight"];
+	else if (commands.hasOwnProperty(msg)) return commands[msg];
 	else return "";
 }
 
 function newCommand(msg) {
 	let args = msg.split(' ');
-	if (args.length < 3) return false;
+	if (args.length < 3) return "Invalid usage! Try: !addcomm command_name response";
 	let newComm = args[1].replace("!", "");
-	if (newComm == "commands") return false;
+	if (newComm == "commands") return "Do not try to overwrite the !commands command >:C";
 	args = args.splice(2);
+	if (!commands.hasOwnProperty("!" + newComm)) commands["!commands"] += ", !" + newComm;
 	commands["!" + newComm] = args.join(" ");
-	if (commands.hasOwnProperty("!" + newComm)) commands["!commands"] += ", !" + newComm;
 	updateCommands();
-	return true;
+	return "Added or updated command!";
 }
 
 function delCommand(msg) {
 	let args = msg.split(' ');
-	let delComm = args[1];
-	if (delComm == "commands") return false;
+	let delComm = args[1].replace("!", "");
+	if (delComm == "commands" || delComm == "demoknight") return "This command cannot be deleted >:C";
 	if (commands.hasOwnProperty("!" + delComm)) {
 		delete commands["!" + delComm];
 		commands["!commands"] = commands["!commands"].replace(", !" + delComm, "");
 		updateCommands();
-		return true;
+		return "Deleted command!";
 	}
-	else return false;
+	else return "Command does not exist!";
 }
 
 function updateCommands() {
