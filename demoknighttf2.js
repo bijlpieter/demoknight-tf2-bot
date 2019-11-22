bb1b78fee0b98c323de05736ace2558b842af410
Chrysophylaxs <43391350+Chrysophylaxs@users.noreply.github.com>
2019-11-22 18:35:55 +0100

    Add files via upload

--- demoknighttf2.js
+++ demoknighttf2.js
@@ -46,12 +46,15 @@ dclient.on("ready", function() {
 
 tclient.on('chat', (channel, userstate, message, self) => {
 	if (message.startsWith("!") && !self) {
-		let response = handleCommand(message.toLowerCase(), channel, userstate['display-name'], userstate.badges.hasOwnProperty('moderator') || userstate.badges.hasOwnProperty('broadcaster'));
+		let response = "";
+		if (userstate.badges == null) response = handleCommand(message.toLowerCase(), channel, userstate['display-name'], false);
+		else response = handleCommand(message.toLowerCase(), channel, userstate['display-name'], userstate.badges.hasOwnProperty('moderator') || userstate.badges.hasOwnProperty('broadcaster'));
 		if (response) tclient.say(channel, response);
 	}
 });
 
 dclient.on('message', (message) => {
+	if (message.content == "!clip") return message.channel.send("Can't clip discord!");
 	if (message.content.startsWith("!") && !message.author.bot) {
 		let response = handleCommand(message.content.toLowerCase(), "#solarlighttf2", message.member.displayName, message.member.hasPermission('MANAGE_MESSAGES', true, true, true));
 		if (response) message.channel.send(response);
@@ -62,6 +65,7 @@ dclient.on('message', (message) => {
 function handleCommand(msg, channel, name, mod) {
 	if (msg.startsWith('!addcomm') && mod) return newCommand(msg, channel);
 	else if (msg.startsWith('!delcomm') && mod) return delCommand(msg, channel);
+	else if (msg == "!clip") return createClip(channel);
 	else if (msg.startsWith("!demoknight")) return name + " has praised the holy demoknight team fortress 2";
 	else if (commands[channel].hasOwnProperty(msg)) return commands[channel][msg];
 	else return "";
@@ -71,7 +75,7 @@ function newCommand(msg, channel) {
 	let args = msg.split(' ');
 	if (args.length < 3) return "Invalid usage! Try: !addcomm command_name response";
 	let newComm = args[1].replace("!", "");
-	if (newComm == "commands") return "Do not try to overwrite the !commands command >:C";
+	if (newComm == "commands" || newComm == "demoknight" || newComm == "clip") return "Do not try to overwrite the !commands command >:C";
 	args = args.splice(2);
 	if (!commands[channel].hasOwnProperty("!" + newComm)) commands[channel]["!commands"] += ", !" + newComm;
 	commands[channel]["!" + newComm] = args.join(" ");
@@ -82,7 +86,7 @@ function newCommand(msg, channel) {
 function delCommand(msg, channel) {
 	let args = msg.split(' ');
 	let delComm = args[1].replace("!", "");
-	if (delComm == "commands" || delComm == "demoknight") return "This command cannot be deleted >:C";
+	if (delComm == "commands" || delComm == "demoknight" || delComm == "clip") return "This command cannot be deleted >:C";
 	if (commands[channel].hasOwnProperty("!" + delComm)) {
 		delete commands[channel]["!" + delComm];
 		commands[channel]["!commands"] = commands[channel]["!commands"].replace(", !" + delComm, "");
@@ -94,4 +98,48 @@ function delCommand(msg, channel) {
 
 function updateCommands() {
 	req({method: "PUT", uri: process.env.COMMANDS, json: commands});
+}
+
+const clipped = new Set();
+
+function createClip(channel) {
+	if (!clipped.has(channel)) {
+		clipped.add(channel);
+		setTimeout(() => {clipped.delete(channel);}, 30000);
+		let uri = "https://api.twitch.tv/kraken/users?login=" + channel.replace("#", "");
+		tclient.api({
+			url: uri,
+			method: "GET",
+			headers: {
+				"Accept": "application/vnd.twitchtv.v5+json",
+				"Client-ID": process.env.CLIENTID
+			}
+		}, (err, res, body) => {
+			if (err) {
+				tclient.say(channel, "An error occurred!");
+				return;
+			}
+			let id = body.users[0]._id;
+			tclient.api({
+				url: "https://api.twitch.tv/helix/clips?broadcaster_id=" + id,
+				method: "POST",
+				headers: {
+					"Accept": "application/vnd.twitchtv.v5+json",
+					"Client-ID": process.env.CLIENTID,
+					"Authorization": process.env.BEARER
+				}
+			}, (err, res, body) => {
+				if (body.hasOwnProperty('error')) {
+					tclient.say(channel, body.message);
+				}
+				else {
+					let clipID = body.data[0].id
+					console.log("https://clips.twitch.tv/" + clipID);
+					tclient.say(channel, "https://clips.twitch.tv/" + clipID);
+				}
+			});
+		});
+		return "";
+	}
+	else return "A clip was recently made by somebody else!";
 }
